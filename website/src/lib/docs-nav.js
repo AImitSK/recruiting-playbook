dae0385bf0594da35468d234b96c3c92fb0c2209
Stefan Kuehne <s.kuehne@sk-online-marketing.de>
2026-02-08 15:24:44 +0100

    Docs(Website): Remove outdated Templates documentation

--- website/src/lib/docs-nav.js
+++ website/src/lib/docs-nav.js
@@ -2,7 +2,6 @@ export const docsNav = [
   { slug: 'getting-started', title: 'Erste Schritte' },
   { slug: 'shortcodes', title: 'Shortcodes' },
   { slug: 'gutenberg-blocks', title: 'Gutenberg Blocks' },
-  { slug: 'templates', title: 'Templates' },
   { slug: 'hooks', title: 'Hooks & Filter' },
   { slug: 'email', title: 'E-Mail-Konfiguration' },
   { slug: 'spam-protection', title: 'Spam-Schutz' },
